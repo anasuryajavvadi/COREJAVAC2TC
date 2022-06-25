a26b042d4fd4368e3d6ac685977c77a6b142d06e
acer <acer@192.168.0.115>
2022-06-25 15:14:33 +0530

    terenory

---
+++ SVKP/src/Demoternary.java
@@ -0,0 +1,13 @@
+
+public class Demoternary {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub 
+		{
+			int x = 11;
+			x = (10 == x)? 1:0; 
+			System.out.println(x);
+		}
+	}
+
+}
